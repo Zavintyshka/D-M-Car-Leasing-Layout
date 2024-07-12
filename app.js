8ff3b5b202d3812dd4676635fec04cdf27165a85
Vadim Sosnin <VadimSosnin02@yandex.ru>
2024-07-12 14:42:36 +0300

    Added adaptive layout for new_cars and contacts html pages

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const swiper = new Swiper('.swiper', {speed: 1200,
-    autoplay: {delay:3000},
-    allowTouchMove:false,
-    loop: true,
-    pagination: {el: '.swiper-pagination'}});
-
 const open_menu_burger_button = document.querySelector(".header_menu__burger_open_button")
 const close_menu_burget_button = document.querySelector(".header_menu__burger_close_button")
 
@@ -22,5 +16,3 @@ const close_burger_menu = function(){
 
 open_menu_burger_button.addEventListener("click", open_burger_menu)
 close_menu_burget_button.addEventListener("click", close_burger_menu)
-
-
